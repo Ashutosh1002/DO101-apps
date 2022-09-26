94e4a10a30400439c3f0e4bc20f8efef03cde6a4
Ashutosh1002 <114012588+Ashutosh1002@users.noreply.github.com>
2022-09-26 12:19:15 +0530

    hellomars

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,18 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/mars', function (req, res) {
+  res.send('Hello Mars!\n');
+});
+
+app.get('/bilal', function (req, res) {
+  res.send('Hello billu!\n');
+});
+
+app.get('/siwan', function (req, res) {
+  res.send('Hello Siwan!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
